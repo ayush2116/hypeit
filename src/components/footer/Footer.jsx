16cba66a0ee1e70c7040ace6d71ea2cb1489d222
ayush2116 <ayushforever21@gmail.com>
2022-12-25 12:42:29 +0530

    sdgsd

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./footer.css"
-import Logo from "../../assets/blackie.png"
+import Logo from "../../assets/blackish.png"
 const Footer = () => {
   return (
     <footer className='footer'>
